fa70eca22a6911ade1f4f10a43ff6b0fbdb96d36
Diyorbek <diyorbeksobirov148gmail.com>
2022-05-30 12:32:26 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// let counts = setInterval(updated, 40);
+// let upto = 0;
+// let width = 0;
+// function updated(){
+//     const count = document.getElementById("conter");
+//     const hr = document.getElementById("hr");
+//     ++upto;
+//     count.innerHTML = "LOADING" + upto + "%";
+//     hr.style.width = upto + "%";
+//     if (upto === 100) {
+//         clearInterval(counts);
+//         count.innerHTML = "DONE!";
+//     }
+// }
